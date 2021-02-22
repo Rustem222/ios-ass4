db8bb92909d88c87b876af3a7427edc66a369a95
Rustem222 <53113123+Rustem222@users.noreply.github.com>
2021-02-22 22:59:16 +0600

    Add files via upload

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Rustem Orazbek on 22.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

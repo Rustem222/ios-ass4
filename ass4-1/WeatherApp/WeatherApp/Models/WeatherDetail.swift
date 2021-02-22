bcb211879e5cf07b7187741143bbe89de4d3984f
Rustem222 <53113123+Rustem222@users.noreply.github.com>
2021-02-22 23:00:03 +0600

    Add files via upload

---
+++ ass4-1/WeatherApp/WeatherApp/Models/WeatherDetail.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherDetail.swift
+//  WeatherApp
+//
+//  Created by Rustem Orazbek on 22.02.2021.
+//
+
+import Foundation
+
+struct WeatherDetail: Codable {
+    var main: String
+    var description: String
+    var icon: String
+}
---
+++ ass4-1/WeatherApp/WeatherApp/Networking/API.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  WeatherApp
+//
+//  Created by Rustem Orazbek on 22.02.2021.
+//
+
+import Foundation
+
+struct API {
+    static let key = ""
+}
